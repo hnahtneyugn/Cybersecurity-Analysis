98ecb656e54099a0669fcea52c85af66d5920255
hnahtneyugn <tanguyenthanh1969@gmail.com>
2025-10-14 23:12:55 +0700

    use Terraform to provision resources in GCP

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "credentials" {
+  description = "My Credentials"
+  type        = string
+}
+
+variable "project" {
+  description = "My Main Project"
+  type        = string
+}
+
+variable "region" {
+  description = "Project Region"
+  type        = string
+}
+
+variable "location" {
+  description = "Project Location"
+  type        = string
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  type        = string
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  type        = string
+}
+
